7af4a56d51b62131236b9461ee10af4304240777
Dabin <dorothy9795@gmail.com>
2021-01-02 00:57:32 +0900

    씬파일 제거

--- SaveData.gd
+++ SaveData.gd
@@ -3,7 +3,7 @@ extends Node
 class_name SaveData
 
 var data: Dictionary = {
-		"version_code" : SaveDataManager.Version.BETA2,
+		"version_code" : SaveDataManager.Version.ALPHA,
 		"option": {
 			"bgm_volume": 0,
 			"effect_volume": 0, 
